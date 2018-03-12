b7a59fc677f00597354721e75358a29d86b75046
kbuffardi <kbuffardi@ecc-jaguar.csuchico.edu>
2018-03-11 23:54:09 -0700

    Basic starter main

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "SecretDoor/SecretDoor.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  SecretDoor game;
+
+  //Recall the SecretDoor functions you may need:
+  // game.newGame(); //indicates the start to a new game
+  // game.guessDoorA(); //tells host the player guesses Door A
+  // game.guessDoorB(); //tells host the player guesses Door B
+  // game.guessDoorC(); //tells host the player guesses Door C
+  // game.isAopen(); //returns whether A is revealed to NOT have prize
+  // game.isBopen(); //returns whether B is revealed to NOT have prize
+  // game.isCopen(); //returns whether C is revealed to NOT have prize
+  // game.isWinner(); //returns whether the player's second guess had the prize
+  return 0;
+}
